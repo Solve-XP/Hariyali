9725d585f5396c7c24e0806bfd83987a058b06d6
balu-solvexp <balu.gorad@solvexp.in>
2026-05-12 14:16:10 +0530

    fix: remove incorrectly pushed app folder

--- app/db/indexes.py
+++
@@ -1,9 +0,0 @@
-from app.db.database import users_collection
-
-
-async def create_indexes():
-
-    await users_collection.create_index(
-        "phone",
-        unique=True
-    )
--- app/schemas/user.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class UserOut(BaseModel):
-    id: str
-    name: str
-    phone: str
-    status: str
